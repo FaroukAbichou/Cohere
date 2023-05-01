d7cf339c55bbf5ade07d0e0d10a4cb0b74f23246
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-05-01 01:51:18 +0100

    Initial Commit

---
+++ site/src/jsMain/kotlin/org/kobe/Farouk/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package org.kobe.Farouk.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
---
+++ site/src/jsMain/kotlin/org/kobe/Farouk/pages/About.kt
@@ -0,0 +1,18 @@
+package org.kobe.Farouk.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.navigation.Link
+import org.kobe.Farouk.components.layouts.PageLayout
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
+
+@Page
+@Composable
+fun AboutPage() {
+    PageLayout("ABOUT") {
+        Text("This is a skeleton app used to showcase a basic site made using Kobweb")
+        P()
+        Link("/", "Go Home")
+    }
+}
